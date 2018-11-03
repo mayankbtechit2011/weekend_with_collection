333fd18f243bd39d637bec8fe6a0c90c79807430
Mayank Singh 10038967 <mayank.singh@gemalto.com>
2018-11-03 14:27:43 +0530

    Using collections

--- src/java/com/nov/FindDistinctNumbers.java
+++ src/java/com/nov/FindDistinctNumbers.java
@@ -1,15 +1,14 @@
-package com.nov;
+package java.com.nov;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Duplicate {
-
+public class FindDistinctNumbers {
 
 	public static void main(String[] args) {
-		
+
 		List<Integer> numerList = new ArrayList<>();
-		
+
 		numerList.add(10);
 		numerList.add(20);
 		numerList.add(30);
@@ -22,16 +21,16 @@ public class Duplicate {
 		numerList.add(30);
 
 		List<Integer> list = getlistWithoutDuplicates(numerList);
-	
+
 		System.out.println("Distinct List: : "+list);
 	}
 
 	public static List<Integer> getlistWithoutDuplicates(List<Integer> list){
-		
+
 		List<Integer> distinctList = new ArrayList<>();
-		
+
 		for (Integer item: list) {
-		
+
 			if(!distinctList.contains(item))
 			{
 				distinctList.add(item);
@@ -39,7 +38,5 @@ public class Duplicate {
 		}	
 
 		return distinctList;
-
-
 	}
 }
